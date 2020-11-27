7f1048e19c4a4de60ea4d93319ce0cbbddce73c1
Stephane Candelas <stephanecandelas@gmail.com>
2020-11-27 23:15:03 +0000

    test Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React from "react";
 import cards from "../img/cards_cover/honor_heart-14.png";
 
 const Header = () => {
+	// console.log(cards);
 	return (
 		<div className="header-container">
 			<div className="header-img-container">
-				<img src={cards} alt="heart cards" />
+				<img src={cards} alt="heart card" />
 			</div>
 			<div className="header-title-container">
 				<h2 className="header-title">Memory Cards</h2>
--- src/test/Header.test.js
+++ src/test/Header.test.js
@@ -2,10 +2,19 @@ import React from "react";
 import Header from "../components/Header";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import cards from "../img/cards_cover/honor_heart-14.png";
 
 describe("<Header />  render correctly", () => {
 	it("should display Memory Cards as a title", () => {
 		render(<Header />);
-		screen.debug();
+		// screen.debug();
+		expect.assertions(1);
+		screen.getByRole("img");
+		screen.getByRole("heading");
+		screen.getByText(/Memory cards/i);
+		screen.getByAltText(/heart card/i);
+		expect(screen.getByRole("heading")).toHaveTextContent(
+			("text", /Memory cards/i)
+		);
 	});
 });
